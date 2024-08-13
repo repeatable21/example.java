ad1cb99291322df73e0d3f59495972e31e366d98
repeatable21 <qcvjkhuwq@outlook.com>
2024-08-13 16:18:06 +0800

    Create enample.java

---
+++ enample.java
@@ -0,0 +1,26 @@
+# -*- coding: UTF-8 -*-
+
+# Filename : helloworld.py
+# author by : www.runoob.com
+
+# 该实例输出 Hello World!
+print('Hello World!')
+#!/usr/bin/python3
+
+counter = 100          # 整型变量
+miles   = 1000.0       # 浮点型变量
+name    = "runoob"     # 字符串
+
+print (counter)
+print (miles)
+print (name)
+num_int = 123
+num_flo = 1.23
+
+num_new = num_int + num_flo
+
+print("num_int 数据类型为:",type(num_int))
+print("num_flo 数据类型为:",type(num_flo))
+
+print("num_new 值为:",num_new)
+print("num_new 数据类型为:",type(num_new))
